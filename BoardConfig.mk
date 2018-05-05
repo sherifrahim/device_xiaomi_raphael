3a95c4b99e6a8e5361bd3c2ed5f5b71d4806fcab
Chris Fries <cfries@google.com>
2018-05-04 21:38:44 -0500

    raphael: Set TARGET_ENABLE_MEDIADRM_64
    Use 64 bit crypto libraries for DRM
    
    Bug: 77822369
    Change-Id: Ic3309d0c3bd82a37b52c5cb765b007ab2ae2555b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,6 +85,9 @@ TARGET_USES_ION := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
+# DRM
+TARGET_ENABLE_MEDIADRM_64 := true
+
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
