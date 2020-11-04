255fd14daad1a0f98a67bf17982b036b9dd07784
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-11-04 15:04:40 +0500

    raphael: Use Pixel offline charging animation
    * from redfin-user RD1A.201105.003.A1-6886512
    
    Change-Id: I25e5b0e8cb6e463fb273227482461692ab8b6272

--- device.mk
+++ device.mk
@@ -359,6 +359,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.perf@2.2.vendor
 
+# Pixel Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
