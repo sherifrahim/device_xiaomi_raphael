26a2e1b4b25a13574a31237c782c3549f1488bff
Jorge Lucas <jorgelucas@pixelexperience.org>
2021-04-14 00:50:27 -0300

    raphael: Add missing TARGET_BOOT_ANIMATION_RES
    Change-Id: I3879ff51a5484097ca073997599e754363eaa6ef

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -23,6 +23,7 @@ TARGET_USES_BLUR := true
 TARGET_GAPPS_ARCH := arm64
 TARGET_INCLUDE_STOCK_ARCORE := true
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080p
 
 PRODUCT_PRODUCT_PROPERTIES += \
   ro.ssos.cpu=SD855
