5622cfc4d25dfe88d7e6c45913a398ccec741e01
Sherif Rahim <sherifrahim2001@gmail.com>
2021-04-22 10:36:43 +0000

    raphael:Nezuko's Time
    * nuked Pixel Charger
    * Use Selinux Neverallow Flag for now

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -13,9 +13,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ssos_raphael.mk
+    $(LOCAL_DIR)/aosp_raphael.mk
 
 COMMON_LUNCH_CHOICES := \
-    ssos_raphael-user \
-    ssos_raphael-userdebug \
-    ssos_raphael-eng
+    aosp_raphael-user \
+    aosp_raphael-userdebug \
+    aosp_raphael-eng
