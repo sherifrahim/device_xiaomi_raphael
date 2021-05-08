4331030c0d613c44e19229efbcbdaed7246185f3
Sherif Rahim <sherifrahim2001@gmail.com>
2021-05-08 18:51:46 +0000

    rahael:Build CustomDoze
    *Stop using Xiaomi Doze

--- device.mk
+++ device.mk
@@ -169,6 +169,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.opengles.aep.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.opengles.aep.xml
 
+# Doze
+PRODUCT_PACKAGES += \
+    CustomDoze
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
