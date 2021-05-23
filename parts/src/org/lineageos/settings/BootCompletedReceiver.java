96249b0f15751622401b822724f4e9b0ffa5f087
Adhitya Mohan <me@adhityamohan.in>
2021-05-23 15:27:42 +0530

    raphael: parts: Don't crash the whole process if dirac doesn't start
    * during dev stuff we may nuke dirac sometimes, don't crash parts because of it

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -39,10 +39,14 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG)
-            Log.d(TAG, "Received boot completed intent");
-        DozeUtils.checkDozeService(context);
+        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        try {
+        DiracUtils.initialize(context);
+        } catch (Exception e) {
+            Log.d(TAG, "Dirac is not present in system");
+        }
         PopupCameraUtils.startService(context);
+        DozeUtils.checkDozeService(context);
         ThermalUtils.startService(context);
 
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
