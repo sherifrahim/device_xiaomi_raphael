233117de6222b6adf574debde79429b7c290a00b
Sherif Rahim <sherifrahim2001@gmail.com>
2021-09-13 23:52:50 +0530

    raphael: Hello Radiant !

--- device.mk
+++ device.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/xiaomi/raphael/raphael-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-arrow
+    $(LOCAL_PATH)/overlay-radiant
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
--- radiant_raphael.mk
+++ radiant_raphael.mk
@@ -12,22 +12,30 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Arrow stuff.
-$(call inherit-product, vendor/arrow/config/common.mk)
+PRODUCT_CHARACTERISTICS := nosdcard
+
+# Inherit some common radiant stuff.
+RADIANT_BUILD_TYPE := OFFICIAL
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_USES_BLUR := true
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+$(call inherit-product, vendor/radiant/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.radiant.cpu=SD855
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := arrow_raphael
+PRODUCT_NAME := radiant_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi K20 Pro
+PRODUCT_MODEL := MI 9T Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-DEVICE_MAINTAINER := TH779, kubersharma001
-
-PRODUCT_CHARACTERISTICS := nosdcard
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="raphael"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 BUILD_FINGERPRINT := Xiaomi/raphael/raphael:11/RKQ1.200826.002/V12.5.2.0.RFKMIXM:user/release-keys
-
-TARGET_INCLUDE_PIXEL_CHARGER := true
